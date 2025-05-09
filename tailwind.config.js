51c27d03e320f39a7b16a20839c6e55baf5260ee
aimpact-aim <Gsoftideas@gmail.com>
2025-05-09 09:51:14 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#007bff",
+        secondary: "#6c757d",
+        accent: "#28a745",
+      },
+    },
+  },
+  plugins: [],
+};
